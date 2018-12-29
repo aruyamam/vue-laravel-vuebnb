b966261e6ea5c1fb778684a2c785436fafbedcdc
aruyamam <aruyamam@gmail.com>
2018-12-29 18:19:34 +0900

    Image modal window

--- app.js
+++ app.js
@@ -9,6 +9,31 @@ var app = new Vue({
       },
       amenities: sample.amenities,
       prices: sample.prices,
-      contracted: true
+      contracted: true,
+      modalOpen: false
+   },
+   methods: {
+      escapeKeyListener(evt) {
+         if (evt.keyCode === 27 && this.modalOpen) {
+            this.modalOpen = false;
+         }
+      }
+   },
+   watch: {
+      modalOpen() {
+         const className = 'modal-open';
+
+         if (this.modalOpen) {
+            document.body.classList.add(className);
+         } else {
+            document.body.classList.remove(className);
+         }
+      }
+   },
+   created() {
+      document.addEventListener('keyup', this.escapeKeyListener);
+   },
+   destroyed() {
+      document.removeEventListener('keyup', this.escapeKeyListener);
    }
 });
